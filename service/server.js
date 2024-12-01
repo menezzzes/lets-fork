64623cd9810c42eaa3483528cc8bb079b8d85402
Laura Marques <lauramarques@Lauras-MacBook-Air.local>
2024-12-01 15:23:40 -0300

    Updated CORS settings

--- service/server.js
+++ service/server.js
@@ -2,13 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
-
 const app = express();
+
+
 app.use(cors()); 
 app.use(bodyParser.json());
 
-// Connect to MongoDB using the environment variable
+
+app.use((req, res, next) => { 
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); next(); });
+
+
+  // Connect to MongoDB using the environment variable
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
